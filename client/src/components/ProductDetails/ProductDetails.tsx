eb97ddcb7800ad1cae4d715ef70cf237f714fa6a
Rafael Falk <rbrtrflflk@gmail.com>
2022-05-17 18:27:38 +0200

    fe: edit button

--- client/src/components/ProductDetails/ProductDetails.tsx
+++ client/src/components/ProductDetails/ProductDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Product from '../../utilities/types/Product.types';
 import './ProductDetails.css';
+import { ReactComponent as AiFillEdit } from '../../assets/AiFillEdit.svg';
 
 type Props = {
   product: Product
@@ -9,18 +10,24 @@ type Props = {
 function ProductDetails({ product }: Props) {
   return (
     (product) && (
-      <div className="productdetails">
-        <div className="productdetails-image-container">
-          <img className="productdetails-image" src={product.image} alt={product.name} />
-        </div>
-        <div className="productdetails-divider" />
-        <div className="productdetails-details">
-          <div className="productdetails-text">
-            <h3>{product.brand}</h3>
-            <h3>{product.name}</h3>
+      <div className="productdetails-container">
+        <div className="productdetails">
+          <div className="productdetails-image-container">
+            <img className="productdetails-image" src={product.image} alt={product.name} />
           </div>
-          {(product.tags !== '') && (
-          <h4 className={`productdetails-tags-${product.tags.toLowerCase()}`}>{product.tags}</h4>)}
+          <div className="productdetails-divider" />
+          <div className="productdetails-details">
+            <div className="productdetails-text">
+              <h3>{product.brand}</h3>
+              <h3>{product.name}</h3>
+            </div>
+            <AiFillEdit className="productdetails-button" />
+          </div>
+          <div className="productdetails-description">
+            Description Description Description Description
+            Description Description Description Description
+            Description Description
+          </div>
         </div>
       </div>
     )
